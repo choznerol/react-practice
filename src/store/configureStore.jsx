d81453c102339ad9226a294c1b0585e3bca17e1b
choznerol <b02611002@ntu.edu.tw>
2017-09-28 12:54:11 +0800

    Migrate react-redux for hot reloading reducers
    release note: https://github.com/reactjs/react-redux/releases/tag/v2.0.0

--- src/store/configureStore.jsx
+++ src/store/configureStore.jsx
@@ -2,5 +2,15 @@ import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
 
 export default function configureStore(initialState) {
-    return createStore(rootReducer, initialState);
+  const store = createStore(rootReducer, initialState);
+
+  if (module.hot) {
+    // Enable Webpack hot module replacement for reducers
+    module.hot.accept('../reducers', () => {
+      const nextRootReducer = require('../reducers/index');
+      store.replaceReducer(nextRootReducer);
+    });
+  }
+
+  return store;
 }
