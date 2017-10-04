a1e31c359cdbf7ec1aff67e1b24c38f3e315da7b
choznerol <b02611002@ntu.edu.tw>
2017-10-04 19:50:26 +0800

    Add new presentational components

---
+++ src/components/AbilityCounter.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const AbilityCounter = ({ abilityType, unsavedAbility, onIncrementClick, onDecrementClick, remainPoints }) => {
+    return (
+        <span className="d-flex justify-content-between mb-2">
+            <b style={{width: '4rem'}}>{ abilityType.toUpperCase() }</b>
+            <button
+                className="btn btn-primary"
+                onClick={ onIncrementClick }
+                disabled={ remainPoints <= 0 }
+                > + </button>
+            <b>
+                { unsavedAbility }
+            </b>
+            <button
+                className="btn btn-primary"
+                onClick={ onDecrementClick }
+                disabled={ unsavedAbility <= 0 }
+                > - </button>
+        </span>
+    )
+}
+
+AbilityCounter.propTypes = {
+    abilityType: PropTypes.string.isRequired,
+    remainPoints: PropTypes.number.isRequired,
+    unsaved_ability: PropTypes.number.isRequired,
+    onDecrementClick: PropTypes.func.isRequired,
+    onDecrementClick: PropTypes.func.isRequired
+}
+
+export default AbilityCounter
---
+++ src/components/FetchingProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FetchingProfile = () => (
+    <div className="card">
+        <div className="card-body">
+            <h1>FetchingProfile...</h1>
+        </div>
+    </div>
+)
+
+export default FetchingProfile;
