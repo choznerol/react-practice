b2bcb275c045256821ef2e43db5ab44303046084
choznerol <b02611002@ntu.edu.tw>
2017-10-14 00:52:08 +0800

    新增頂端進度條
    使用套件：react-redux-loading-bar
    https://github.com/mironov/react-redux-loading-bar

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 import 'rxjs';
 import HeroPage from './components/HeroPage';
 import Message from './containers/Message';
+import LoadingBar from 'react-redux-loading-bar'
 import './styles/index.scss'
 
 const store = configureStore();
@@ -14,19 +15,27 @@ class App extends Component {
         return (
             <Provider store={store}>
                 <BrowserRouter>
-                    <main className="p-2">
+                    <div>
+
+                        {/* 頂部進度條 */}
+                        <LoadingBar/>
+
                         {/* 更新成功、失敗提示訊息 */}
                         <Message/>
 
-                        <Switch>
-                            <Route path="/heros" component={HeroPage}></Route>
+                        {/*  */}
+                        <main className="p-2">
+                            <Switch>
+                                <Route path="/heros" component={HeroPage}></Route>
 
-                            {/* Default redirect to /heros */}
-                            <Route render={() => (
-                                <Redirect to='/heros'/>
-                            )}></Route>
-                        </Switch>
-                    </main>
+                                {/* Default redirect to /heros */}
+                                <Route render={() => (
+                                    <Redirect to='/heros'/>
+                                )}></Route>
+                            </Switch>
+                        </main>
+
+                    </div>
                 </BrowserRouter>
             </Provider>
         );
--- src/reducers/index.jsx
+++ src/reducers/index.jsx
@@ -1,9 +1,11 @@
-import {combineReducers} from 'redux';
+import { combineReducers } from 'redux'
+import { loadingBarReducer } from 'react-redux-loading-bar'
 import heros from './heros'
 import profiles from './profiles'
 import message from './message'
 
 const rootReducer = combineReducers({
+    loadingBar: loadingBarReducer,
     heros,
     profiles,
     message
