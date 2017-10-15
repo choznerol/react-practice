bb053f73fdba5fe80f386c08c0de7aefaf0893de
choznerol <b02611002@ntu.edu.tw>
2017-10-15 12:10:55 +0800

    refactor: 簡化 HeroList 元件內部邏輯（移至 mapStateToProps）

--- src/containers/HeroProfile.jsx
+++ src/containers/HeroProfile.jsx
@@ -1,15 +1,19 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { increaseUnsavedAbility, decreaseUnsavedAbility,
-        fetchProfileIfNeeded, patchProfile, clearMessage } from '../actions'
+import { increaseUnsavedAbility,
+         decreaseUnsavedAbility,
+         fetchProfileIfNeeded,
+         patchProfile,
+         clearMessage } from '../actions'
 import PropTypes from 'prop-types'
 import LoadingCard from '../components/LoadingCard'
 import AbilityCounter from '../components/AbilityCounter'
+import PulseLoader from '../components/PulseLoader'
 
 class HeroProfile extends Component {
     static propTypes = {
         heroID: PropTypes.string.isRequired,
-        isFetching: PropTypes.bool.isRequired,
+        showLoading: PropTypes.bool.isRequired,
         handleIncrementClick: PropTypes.func.isRequired,
         handleDecrementClick: PropTypes.func.isRequired,
         handleHeroChanged: PropTypes.func.isRequired,
@@ -27,47 +31,50 @@ class HeroProfile extends Component {
     }
 
     render () {
-        const { heroID, profiles, isFetching, handleIncrementClick, handleDecrementClick, handleSave } = this.props
 
-        if (isFetching) {
+        if (this.props.showLoading) {
             return (<LoadingCard/>)
         } else {
-            const abilities = profiles.items[heroID]
-            const remainPoints = this.calRemainPoints(abilities)
 
             return (
                 <div className="card">
                     <div className="card-body">
                         <div className="row">
+
                             {/* 右半邊：可調整 4 種能力值 */}
                             <div className="col">
                                 { ['str', 'int', 'agi', 'luk'].map(key => {
                                         return (
                                             <AbilityCounter
-                                                key={ heroID + key }
+                                                key={ this.props.heroID + key }
                                                 abilityType={ key }
-                                                remainPoints={ remainPoints }
-                                                unsavedAbility={ abilities[`unsaved_${ key }`] }
-                                                onIncrementClick={ () => handleIncrementClick(heroID, key)}
-                                                onDecrementClick={ () => handleDecrementClick(heroID, key)}
+                                                remainPoints={ this.props.remainPoints }
+                                                unsavedAbility={ this.props.profile[`unsaved_${ key }`] }
+                                                onIncrementClick={ () => this.props.handleIncrementClick(this.props.heroID, key)}
+                                                onDecrementClick={ () => this.props.handleDecrementClick(this.props.heroID, key)}
                                             />
                                         )
                                     })}
                             </div>
+
                             {/* 左半邊：顯示剩餘點數及儲存 */}
                             <div className="col d-flex flex-column justify-content-end align-items-end">
-                                <b>剩餘點數：{ remainPoints }</b>
+                                <b>剩餘點數：{ this.props.remainPoints }</b>
                                 <button
                                     className="btn btn-primary"
-                                    onClick={() => { handleSave(heroID, {
-                                            str: abilities.unsaved_str,
-                                            int: abilities.unsaved_int,
-                                            agi: abilities.unsaved_agi,
-                                            luk: abilities.unsaved_luk
+                                    onClick={() => { this.props.handleSave(this.props.heroID, {
+                                            str: this.props.profile.unsaved_str,
+                                            int: this.props.profile.unsaved_int,
+                                            agi: this.props.profile.unsaved_agi,
+                                            luk: this.props.profile.unsaved_luk
                                         })
                                     }}
                                 >
-                                    儲存
+                                    {this.props.isSubmitting ? (
+                                        <PulseLoader loading={ true } className="pulse-loader"/>
+                                    ) : (
+                                        '儲存'
+                                    )}
                                 </button>
                             </div>
                         </div>
@@ -76,21 +83,32 @@ class HeroProfile extends Component {
             )
         }
     }
-
-    calRemainPoints(abilities) {
-        const allPoints = ['str', 'int', 'agi', 'luk'].reduce((prev, key) => prev + abilities[key], 0)
-        const usedPoints = ['unsaved_str', 'unsaved_int', 'unsaved_agi', 'unsaved_luk'].reduce((prev, key) => prev + abilities[key], 0)
-        return allPoints - usedPoints
-    }
 }
 
 const mapStateToProps = (state, ownProps) => {
+
     const heroID = ownProps.match.params.id
-    const isFetching = !state.profiles.items[heroID]
-    return {
-        heroID,
-        isFetching,
-        profiles: state.profiles,
+    const profile = state.profiles.items[heroID]
+    const showLoading = !profile || state.profiles.isFetching
+
+    if (showLoading) {
+        return {
+            heroID,
+            showLoading
+        }
+    } else {
+
+        const totalPoints = ['str', 'int', 'agi', 'luk'].reduce((prev, key) => prev + profile[key], 0)
+        const totalUnsavedPoints = ['unsaved_str', 'unsaved_int', 'unsaved_agi', 'unsaved_luk'].reduce((prev, key) => prev + profile[key], 0)
+
+        return {
+            showLoading,
+            heroID,
+            profile,
+            totalPoints,
+            isSubmitting: state.profiles.isSubmitting,
+            remainPoints: totalPoints - totalUnsavedPoints
+        }
     }
 }
 
